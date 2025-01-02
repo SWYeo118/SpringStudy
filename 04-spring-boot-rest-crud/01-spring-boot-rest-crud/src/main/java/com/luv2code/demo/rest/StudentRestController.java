765db99a967e0b95f91de52150d787e3fbf17329
여상우 <effortmeta@gmail.com>
2025-01-02 17:40:59 +0900

    @PostConstruct 어노테이션으로 약간의 리팩터리(한대)

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudent() {
-        List<Student> theStudents = new ArrayList<>();
+
+    private List<Student> theStudents = new ArrayList<>();
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
         theStudents.add(new Student("Poo", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Naru", "Smith"));
-
+    }
+    @GetMapping("/students")
+    public List<Student> getStudent() {
         return theStudents;
     }
 }
